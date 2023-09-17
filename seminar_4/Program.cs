f1bb4872e8ce4ae18d056ddfd3e9dfbf5dade33a
Tendo275 <kulikov.sy@mail.ru>
2023-09-17 17:54:33 +0300

    new

---
+++ seminar_4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PowerCalc { class Program { static void Main(string[] args) { Console.WriteLine(“Введите число A:”); int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(“\nВведите степень B:”);
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (B == 0)
+{
+    Console.WriteLine($“A^B = 1 (A и B равны 0)”);
+}
+else
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result *= A;
+    }
+    Console.WriteLine($“{A}^({B}) = {result}”);
+}
+
+Console.ReadKey();
+}
+}
+}
